e7eed95aa9b4df8773fe4049f7af3a89f310b94c
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-12-26 14:27:23 +0400

    Add timeout for server response

--- src/Pakej/Client.hs
+++ src/Pakej/Client.hs
@@ -5,20 +5,29 @@ import qualified Data.Text.IO as Text
 import qualified Data.Text.Encoding as Text
 import           Network
 import           System.Directory (getAppUserDataDirectory)
+import           System.Exit (exitFailure)
 import           System.FilePath ((</>))
 import           System.IO (hPutStrLn, hClose)
+import           System.Timeout (timeout)
 
 
 client :: HostName -> PortID -> String -> IO ()
 client n p o = do
-  h <- connectTo n p
-  hPutStrLn h o
-  i <- ByteString.hGetLine h
-  hClose h
-  Text.putStrLn (Text.decodeUtf8 i)
+  res <- timeout (5 * second) $ do
+    h <- connectTo n p
+    hPutStrLn h o
+    i <- ByteString.hGetLine h
+    hClose h
+    return (Text.decodeUtf8 i)
+  case res of
+    Nothing -> exitFailure
+    Just m  -> Text.putStrLn m
 
 -- | @\~\/.pakej\/%s@
 appDirectory :: String -> FilePath -> IO FilePath
 appDirectory app filename = do
   dir <- getAppUserDataDirectory app
   return (dir </> filename)
+
+second :: Int
+second = 1000000
