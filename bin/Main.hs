6697adaecb6fd04d5215eaa84ab459a19701c7aa
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-01-18 20:55:36 +0400

    Add version suffix to the pakej binary.
    While I'm at it, also added support recompilation from custom source path

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.Version (showVersion)
 import System.Directory (getAppUserDataDirectory)
 import System.Environment (getArgs)
 import System.FilePath ((</>))
@@ -7,23 +8,31 @@ import System.Info (arch, os)
 import System.Posix.Process (executeFile)
 import Text.Printf (printf)
 
+import Paths_pakej (version)
 
-main :: IO ()
+
+main :: IO a
 main = do
   as <- getArgs
   p  <- program
   case as of
-    ["--recompile"] -> do
-      s <- source
-      executeFile "ghc" True [s, "-o", p, "-O", "-threaded"] Nothing
-    _ ->
-      executeFile p False as Nothing
+    ["--recompile", s] -> recompilePakej (return s) p
+    ["--recompile"]    -> recompilePakej source p
+    _                  -> runPakej p as
 
+-- | Run pakej executable with the specified arguments
+runPakej :: FilePath -> [String] -> IO a
+runPakej path args = executeFile path False args Nothing
 
+-- | Recompile pakej sources and place the result somewhere
+recompilePakej :: IO FilePath -> FilePath -> IO a
+recompilePakej src dst = do
+  s <- src
+  executeFile "ghc" True [s, "-o", dst, "-O", "-threaded"] Nothing
 
 -- | ~/.pakej/pakej-x86_64-linux
 program :: IO FilePath
-program = appDirectory "pakej" (printf "pakej-%s-%s" arch os)
+program = appDirectory "pakej" (printf "pakej-%s-%s-%s" arch os (showVersion version))
 
 -- | ~/.pakej/pakej.hs
 source :: IO FilePath
