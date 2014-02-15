265593954def6914bfc875cf238ff9ce72e7676f
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-02-15 15:55:59 +0400

    Add the Tutorial

---
+++ example/aggregation.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.List (intercalate)
+import Data.String (IsString(..))
+import Data.Text.Lazy (Text)
+import Data.Time (formatTime, getZonedTime)
+import Pakej
+import Prelude hiding ((.), id)
+import System.Locale (defaultTimeLocale)
+
+main :: IO ()
+main = pakej $ private "status" . aggregate
+  [ private "loadavg"  . text (loadavg "/proc/loadavg")
+  , private "datetime" . text datetime
+  ]
+
+loadavg :: FilePath -> IO Text
+loadavg = fmap (fromString . intercalate " → " . take 3 . words) . readFile
+
+datetime :: IO Text
+datetime = fmap format getZonedTime
+ where format       = fromString . formatTime defaultTimeLocale formatString
+       formatString = "%m.%d.%y, %a, %H:%M"
---
+++ example/datetime.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.String (IsString(..))
+import Data.Text.Lazy (Text)
+import Data.Time (formatTime, getZonedTime)
+import Pakej
+import Prelude hiding ((.), id)
+import System.Locale (defaultTimeLocale)
+
+main :: IO ()
+main = pakej $ private "datetime" . text datetime
+
+datetime :: IO Text
+datetime = fmap format getZonedTime
+ where format       = fromString . formatTime defaultTimeLocale formatString
+       formatString = "%m.%d.%y, %a, %H:%M"
