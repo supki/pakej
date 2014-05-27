1fd5be42c1631e088b05508617b5ff0c136723f5
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-05-27 09:50:38 +0400

    Warn when the pakej executable is older than its source files
    While we are at it, also mildly improve the error messages

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,17 +1,26 @@
 {-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
-import Data.Version (showVersion)
-import System.Directory (copyFile, createDirectoryIfMissing, getAppUserDataDirectory)
-import System.Environment (getArgs)
-import System.Exit (exitWith)
-import System.FilePath ((</>), takeDirectory)
-import System.Info (arch, os)
-import System.Posix.Process (executeFile)
-import System.Process (runProcess, waitForProcess)
-import Text.Printf (printf)
+import           Control.Applicative
+import           Control.Exception (IOException)
+import           Control.Monad
+import           Data.Foldable (for_, traverse_)
+import           Data.List (isSuffixOf)
+import           Data.Maybe (catMaybes)
+import           Data.Traversable (traverse)
+import           Data.Version (showVersion)
+import           System.Directory (copyFile, createDirectoryIfMissing, getAppUserDataDirectory, getDirectoryContents)
+import           System.Environment (getArgs)
+import           System.Exit (exitWith, exitFailure)
+import           System.FilePath ((</>), takeDirectory)
+import           System.Info (arch, os)
+import           System.IO (hPutStrLn, stderr)
+import           System.IO.Error (catchIOError)
+import qualified System.Posix as Posix
+import           System.Process (runProcess, waitForProcess)
+import           Text.Printf (printf)
 
-import Paths_pakej (version, getDataFileName)
+import           Paths_pakej (version, getDataFileName)
 
 
 main :: IO ()
@@ -32,7 +41,7 @@ initPakej = do
 recompilePakej :: [String] -> FilePath -> IO a
 recompilePakej args dst = do
   source <- sourceFile
-  appDir <- appDirectory
+  appDir <- pakejDirectory
   exitWith =<<
     waitForProcess =<<
       runProcess "cabal" (cabalOpts source ++ args) (Just appDir) Nothing Nothing Nothing Nothing
@@ -41,18 +50,55 @@ recompilePakej args dst = do
 
 -- | Run pakej executable with the specified arguments
 runPakej :: [String] -> FilePath -> IO a
-runPakej args path = executeFile path False args Nothing
+runPakej args path =
+  getModificationTime path >>= \case
+    Nothing ->
+      die "Pakej executable is missing! Did you forget to run\n\
+          \    pakej --recompile\n\
+          \?"
+    Just mtime ->
+      pakejDirectory >>= getSources >>= fmap catMaybes . traverse getModificationTime >>= \mtimes -> do
+        for_ (traverse_ (guard . (> mtime)) mtimes :: Maybe ()) $ \_ ->
+          warn "Some source files are newer than the pakej executable\n\
+                \To rebuild it, please run\n\
+                \    pakej --recompile\n\
+                \!"
+        Posix.executeFile path False args Nothing
+
+getSources :: FilePath -> IO [FilePath]
+getSources file = do
+  getUsefulDirectoryContents file >>= fmap concat . traverse getSources
+ `catchIOError`
+  \_ -> return [file | any (`isSuffixOf` file) [".hs", ".lhs", ".hsc"]]
+{-# ANN getSources "HLint: ignore Redundant do" #-}
+
+getUsefulDirectoryContents :: FilePath -> IO [FilePath]
+getUsefulDirectoryContents dir =
+  (map (\file -> dir </> file) . filter (`notElem` [".", ".."])) <$> getDirectoryContents dir
+
+getModificationTime :: FilePath -> IO (Maybe Posix.EpochTime)
+getModificationTime =
+  handleIOError (\_ -> return Nothing) . fmap (Just . Posix.modificationTime) . Posix.getFileStatus
+
+handleIOError :: (IOException -> IO a) -> IO a -> IO a
+handleIOError = flip catchIOError
+
+die :: String -> IO a
+die m = warn m >> exitFailure
+
+warn :: String -> IO ()
+warn = hPutStrLn stderr
 
 -- | ~/.pakej/pakej-x86_64-linux
 program :: IO FilePath
-program = inAppDirectory (printf "pakej-%s-%s-%s" arch os (showVersion version))
+program = inPakejDirectory (printf "pakej-%s-%s-%s" arch os (showVersion version))
 
 -- | ~/.pakej/pakej.hs
 sourceFile :: IO FilePath
-sourceFile = inAppDirectory "pakej.hs"
+sourceFile = inPakejDirectory "pakej.hs"
 
-inAppDirectory :: FilePath -> IO FilePath
-inAppDirectory path = fmap (</> path) appDirectory
+inPakejDirectory :: FilePath -> IO FilePath
+inPakejDirectory path = fmap (</> path) pakejDirectory
 
-appDirectory :: IO FilePath
-appDirectory = getAppUserDataDirectory "pakej"
+pakejDirectory :: IO FilePath
+pakejDirectory = getAppUserDataDirectory "pakej"
