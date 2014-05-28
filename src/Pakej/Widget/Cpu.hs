f76bbf11ff02da258883739cc52a26b87c03a13e
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-05-28 18:25:17 +0400

    Add cpu widget helper

---
+++ src/Pakej/Widget/Cpu.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- | A parser for @\/proc\/stat@
+module Pakej.Widget.Cpu
+  ( widget
+#ifdef TEST
+  , Snapshot(..)
+  , Diff(..)
+  , parseSnapshot
+  , parseLine
+  , getData
+  , cpuDataError
+  , computeUsage
+  , snapshotDiff
+#endif
+  ) where
+
+import           Control.Category (Category(..))
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.IO.Class (MonadIO(..))
+import           Control.Wire (Wire)
+import           Data.Foldable (asum)
+import           Data.Monoid (Monoid, (<>))
+import           Data.Traversable (traverse)
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Read as Text
+import           Prelude hiding ((.), id)
+import           System.IO.Error (catchIOError)
+
+import           Pakej.Daemon (PakejWidget)
+import           Pakej.Widget (Widget(..), mkFixM, mkState)
+
+-- | Cpu usage snapshot
+newtype Snapshot a = MkS [a] deriving (Show, Eq)
+
+-- | Cpu usage snapshot difference
+newtype Diff a = MkD [a]
+
+-- | Construct a CPU usage widget
+widget
+  :: (Read a, Ord a, Fractional a)
+  => Maybe Int     -- ^ 'Just' a core number or 'Nothing' for the total usage
+  -> PakejWidget a
+widget n = Widget $ usage . readSnapshot n
+
+readSnapshot
+  :: (MonadIO m, Monoid s, Ord a, Fractional a, Read a)
+  => Maybe Int
+  -> Wire s e m b (Snapshot a)
+readSnapshot n =
+  mkFixM $ \_ _ -> liftIO $ do
+    d <- getData "/proc/stat"
+    return $ either (\_ -> Left undefined) (maybe (Left undefined) Right . parseSnapshot n) d
+
+handleIOError :: (IOError -> IO a) -> IO a -> IO a
+handleIOError = flip catchIOError
+
+usage :: (Monoid s, Ord a, Fractional a) => Wire s e m (Snapshot a) a
+usage =
+  mkState emptySnapshot $ \_dt (v, s) -> (Right (computeUsage (snapshotDiff v s)), v)
+
+-- | Compute usage data from the difference of two cpu usage snapshots
+computeUsage :: (Ord a, Fractional a) => Diff a -> a
+computeUsage (MkD xs) =
+  case sum xs of
+    s | abs s < 0.001 -> 0
+      | otherwise     -> sum (map (/ s) (take 3 xs)) * 100
+
+-- | Compute the difference between two cpu usage snapshots
+snapshotDiff :: Num a => Snapshot a -> Snapshot a -> Diff a
+snapshotDiff (MkS xs) (MkS ys) = MkD (zipWith (-) xs ys)
+
+parseSnapshot :: Fractional a => Maybe Int -> Text -> Maybe (Snapshot a)
+parseSnapshot n = asum . map (parseLine n) . Text.lines
+
+emptySnapshot :: Num a => Snapshot a
+emptySnapshot = MkS (repeat 0)
+
+parseLine :: Fractional a => Maybe Int -> Text -> Maybe (Snapshot a)
+parseLine n str = MkS <$> do
+  guard (("cpu" <> maybe " " (Text.pack . show) n) `Text.isPrefixOf` str)
+  traverse (either (const Nothing) (Just . fst) . Text.rational) (drop 1 (Text.words str))
+
+getData :: FilePath -> IO (Either Text Text)
+getData = handleIOError (return . cpuDataError . Text.pack . show) . fmap Right . Text.readFile
+
+cpuDataError :: Text -> Either Text a
+cpuDataError x = Left $ "Pakej.Widget.Cpu: " <> x
---
+++ test/Pakej/Widget/CpuSpec.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Pakej.Widget.CpuSpec
+  ( spec
+  ) where
+
+import Test.Hspec
+
+import Pakej.Widget.Cpu
+
+
+spec :: Spec
+spec = do
+  describe "parseLine" $ do
+    it "parses total cpu usage info when given nothing" $
+      parseLine Nothing "cpu 1 2 3 4" `shouldBe` Just (MkS [1, 2, 3, 4])
+
+    it "parses single core usage info when given it as an argument" $
+      parseLine (Just 0) "cpu0 5 6 7 8" `shouldBe` Just (MkS [5, 6, 7, 8])
+
+    it "does not parse single core usage info when given nothing" $
+      parseLine Nothing "cpu0 1 2 3 4" `shouldBe` noSnapshot
+
+    it "does not parse total cpu usage info when given core number as an argument" $
+      parseLine (Just 0) "cpu 1 2 3 4" `shouldBe` noSnapshot
+
+  describe "parseSnapshot" $ do
+    it "parses the first valid cpu usage record" $
+      parseSnapshot (Just 1)
+        "cpu0 1 2\n\
+        \cpu1 2 3\n\
+        \cpu1 3 4\n" `shouldBe` Just (MkS [2, 3])
+
+    it "fails to parse if valid cpu usage record is available" $
+      parseSnapshot (Just 17)
+        "cpu0 1 2\n\
+        \cpu1 2 3\n\
+        \cpu1 3 4\n" `shouldBe` noSnapshot
+
+  describe "getData" $ do
+    it "does not throw exceptions if file does not exist" $
+      getData "does-not-exist.txt"
+     `shouldReturn`
+      cpuDataError "does-not-exist.txt: openFile: does not exist (No such file or directory)"
+
+    it "does not throw exceptions if file is of the wrong type" $
+      getData "/"
+     `shouldReturn`
+      cpuDataError "/: openFile: inappropriate type (is a directory)"
+
+  describe "computeUsage" $
+    it "computes cpu usage between 2 snapshots" $ do
+      s1 <- parseLine (Just 0) "cpu0 7928261 498437 3288444 106187410 2371849 52378 38421 0 0"
+      s2 <- parseLine (Just 0) "cpu0 7928273 498437 3288455 106187766 2371849 52378 38421 0 0"
+      return (computeUsage (snapshotDiff s1 s2))
+     `shouldBe`
+      Just 6.068601583113456
+
+noSnapshot :: Maybe (Snapshot Rational)
+noSnapshot = Nothing
